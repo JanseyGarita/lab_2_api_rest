7d8f8863fc26d4c3db766935d71352abcd313749
JanseyGarita <janseygarita@gmail.com>
2020-05-18 15:09:02 -0600

    Arreglo de seguridad al crud

--- servidor_api/app/Http/Controllers/personas_controller.php
+++ servidor_api/app/Http/Controllers/personas_controller.php
@@ -19,18 +19,18 @@ class personas_controller extends Controller
     public function insertar_persona(Request $datos)
     {
         $persona = new Persona;
-        $persona->nombre = $datos->nombre;
-        $persona->telefono = $datos->telefono;
-        $persona->correo = $datos->correo;
+        $persona->nombre = ($datos->nombre != null) ? $datos->nombre : $persona->nombre;
+        $persona->telefono = ($datos->telefono != null) ? $datos->telefono : $persona->telefono;
+        $persona->correo = ($datos->correo != null) ? $datos->correo : $persona->correo;
         $persona->save();
         return "Insertado";
     }
     public function actualizar_persona(Request $datos)
     {
         $persona = Persona::find($datos->id);
-        $persona->nombre = $datos->nombre;
-        $persona->telefono = $datos->telefono;
-        $persona->correo = $datos->correo;
+        $persona->nombre = ($datos->nombre != null) ? $datos->nombre : $persona->nombre;
+        $persona->telefono = ($datos->telefono != null) ? $datos->telefono : $persona->telefono;
+        $persona->correo = ($datos->correo != null) ? $datos->correo : $persona->correo;
         $persona->save();
         return "Actualizado";
     }
